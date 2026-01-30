3a06f67f85b96c305905e7921884477acad2c10e
Axel Gutierrez <ingenieroaxelgutierrez@gmail.com>
2026-01-30 13:30:52 -0600

    Update settings.php

--- gestion-tickets/views/dashboard/settings.php
+++ gestion-tickets/views/dashboard/settings.php
@@ -1,5 +1,5 @@
 <?php
-$pageTitle = $pageTitle ?? 'Configuración - RIDECO';
+$pageTitle = $pageTitle ?? 'Configuración - TICKETS';
 include __DIR__ . '/../layouts/header.php';
 require_once __DIR__ . '/../../app/core/Auth.php';
 require_once __DIR__ . '/../../app/helpers/menu.php';
@@ -9,7 +9,7 @@ require_once __DIR__ . '/../../app/helpers/menu.php';
     <!-- Sidebar -->
     <aside class="sidebar" id="sidebar">
         <div class="sidebar-header">
-            <img src="/gestion-tickets/assets/img/favicon_rideco.png" alt="RIDECO">
+            <img src="/gestion-tickets/assets/favicon_rideco.png" alt="ticket">
         </div>
         <?php echo renderMenu(); ?>
     </aside>
@@ -304,5 +304,6 @@ function showMessage(elementId, msg, type) {
     setTimeout(() => msgDiv.style.display = 'none', 5000);
 }
 </script>
+
 
 <?php include __DIR__ . '/../layouts/footer.php'; ?>
